3e191fed1e6dea8c0eba756bc5914c52ace6d3ed
Magyar57 <alxbrp@gmail.com>
2025-08-15 00:44:48 +0200

    Small config changes

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Setup /proc /sys and /dev
+mkdir -p /proc /sys /dev
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+mount -t devtmpfs devtmpfs /dev
+
 while [ 1 ]
 do
     bash
